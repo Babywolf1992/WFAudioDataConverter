8e3e82fdaa256ece510547bc222d20e904447629
babywolf <591138406@qq.com>
2018-01-25 17:41:21 +0800

    init project

---
+++ WFAudioDataConverter/WFAudioDataConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WFAudioDataConverter
+//
+//  Created by babywolf on 2018/1/25.
+//  Copyright © 2018年 babywolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
